fb48e786e023b267b393bd4f451d2a2a84c710e3
Angel39706 <96604953+Angel39706@users.noreply.github.com>
2022-12-19 15:41:47 -0800

    Initial Commit

---
+++ IntroduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroduceStudent
+//
+//  Created by Angel Martinez on 12/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
